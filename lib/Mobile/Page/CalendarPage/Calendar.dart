ff6071f32bad0557f445fe1dcb009897f9dff632
Ugo Santoro <ugo.santoro@epitech.eu>
2021-05-24 19:25:08 +0200

    Move Calendar Page

--- lib/Mobile/Page/CalendarPage/Calendar.dart
+++ lib/Mobile/Page/CalendarPage/Calendar.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter2/Model/Constants.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   CalendarPage({Key key}) : super(key: key);
@@ -7,6 +10,22 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  Map<DateTime, List<Event>> selectedEvents;
+  DateTime day;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat format = CalendarFormat.month;
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
   // Calendar Page
   @override
   Widget build(BuildContext context) {
@@ -21,7 +40,49 @@ class _CalendarPageState extends State<CalendarPage> {
             child: Center(
               child: Container(
                 child: Column(
-                  children: <Widget>[],
+                  children: <Widget>[
+                    TableCalendar(
+                      firstDay: DateTime.utc(2010, 10, 16),
+                      lastDay: DateTime.utc(2030, 3, 14),
+                      focusedDay: _focusedDay,
+                      calendarFormat: format,
+                      startingDayOfWeek: StartingDayOfWeek.monday,
+                      daysOfWeekVisible: true,
+                      onFormatChanged: (CalendarFormat _format) {
+                        setState(() {
+                          format = _format;
+                        });
+                      },
+                      selectedDayPredicate: (day) {
+                        return isSameDay(_selectedDay, day);
+                      },
+                      onDaySelected:
+                          (DateTime selectedDay, DateTime focusedDay) {
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _focusedDay = focusedDay;
+                        });
+                      },
+                      eventLoader: _getEventsfromDay,
+                      calendarStyle: CalendarStyle(
+                        isTodayHighlighted: true,
+                        todayDecoration: BoxDecoration(
+                          color: Constants().focused_color,
+                          shape: BoxShape.rectangle,
+                          //borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        selectedDecoration: BoxDecoration(
+                          color: Constants().selected_color,
+                          shape: BoxShape.rectangle,
+                          //borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      headerStyle: HeaderStyle(
+                        titleCentered: true,
+                        formatButtonShowsNext: false,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
